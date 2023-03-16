75bb2e472daf434d1d6cd4cff824f289fa0a5e92
Galliss13 <matheusgalindo1313@gmail.com>
2023-03-16 09:23:12 -0300

    fix: event type seed error with date ISOString

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -140,8 +140,8 @@ const RoomCaribeen = await prisma.room.createMany({
         title: "Driven.t",
         logoImageUrl: "https://files.driveneducation.com.br/images/logo-rounded.png",
         backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
-        startsAt: dayjs().toDate().toString(),
-        endsAt: dayjs().add(21, "days").toDate().toString(),
+        startsAt: dayjs().toDate().toISOString(),
+        endsAt: dayjs().add(21, "days").toDate().toISOString(),
       },
     });
   }
